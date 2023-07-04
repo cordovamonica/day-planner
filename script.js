4949f056c125d89915e5bfe553fc8490f7f1c0cf
Mona Lisa <cordovamonica.a@gmail.com>
2023-07-04 13:42:31 -0500

    added code to get items from localstorage

--- script.js
+++ script.js
@@ -35,6 +35,16 @@ $(document).ready(function () {
       }
     })
   }
+
+  $("#hour9 .description").val(localStorage.getItem("hour9"));
+  $("#hour10 .description").val(localStorage.getItem("hour10"));
+  $("#hour11 .description").val(localStorage.getItem("hour11"));
+  $("#hour12 .description").val(localStorage.getItem("hour12"));
+  $("#hour1 .description").val(localStorage.getItem("hour1"));
+  $("#hour2 .description").val(localStorage.getItem("hour2"));
+  $("#hour3 .description").val(localStorage.getItem("hour3"));
+  $("#hour4 .description").val(localStorage.getItem("hour4"));
+  $("#hour5 .description").val(localStorage.getItem("hour5"));
 });
   
  
